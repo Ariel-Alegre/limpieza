286dfd9ce22162a6bdbbe382b888bec6f5cb5e0d
Ariel-Alegre <arielalegre98@gmail.com>
2024-01-10 11:34:57 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-
-
-
+import { useEffect } from 'react';
 
 function App() {
-
+  useEffect(() => {
+    // Redirigir a la ruta deseada
+    window.location.href = '/Sofa & Carpet Cleaning.html';
+  }, []);
 
   return (
     <Router>
-        <Routes>
-          <Route path='/' element={<Navigate to='/Sofa & Carpet Cleaning.html' />} />
-         
-
-
-  
+      <Routes>
+        {/* Ruta de inicio */}
+        <Route path='/' element={<Navigate to='/Sofa & Carpet Cleaning.html' />} />
       </Routes>
-
     </Router>
-  )
+  );
 }
 
 export default App;
